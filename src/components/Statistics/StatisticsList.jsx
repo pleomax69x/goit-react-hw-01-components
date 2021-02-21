789ecd29b15c611beb6f18dfc490c76f24ed8b2b
Igor Stuk <independent.frd@gmail.com>
2021-02-21 17:45:19 +0200

    homework1 fix

--- src/components/Statistics/StatisticsList.jsx
+++ src/components/Statistics/StatisticsList.jsx
@@ -3,11 +3,10 @@ import s from './Statistics.module.css';
 
 const bgColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-function StatisticsList({ stats }) {
+function StatisticsList({ stats, title }) {
   return (
     <section className={s.statistics}>
-      <h2 className={s.title}>Upload stats</h2>
-
+      {title && <h2 className={s.title}>{title}</h2>}
       <ul className={s.statList}>
         {stats.map(stat => (
           <li
